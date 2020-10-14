f060fc9cf9f465d3cb345fe34db50fcea0501bd0
Linnea <linnea@klangby.com>
2020-10-14 17:44:56 +0200

    Projektet
    Komplett ursprungsbana att utgå ifrån, saknas 2 levlar och resterande två behöver färdigställas.
    
    Co-Authored-By: AxelP312 <60465685+axelp312@users.noreply.github.com>

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    public float speed = 1.5f;
+    public float jump = 1.5f;
+
+    bool isGrounded;
+    public Transform groundCheck;
+    public Transform groundCheckL;
+    public Transform groundCheckR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        if(Physics2D.Linecast(transform.position, groundCheck.position, 1<< LayerMask.NameToLayer("Ground"))||
+            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground"))||
+            Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            if (isGrounded == true)
+            {
+                animator.Play("Run");
+            }
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            if (isGrounded == true)
+            {
+                animator.Play("Run");
+            }
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            if (isGrounded == true)
+            {
+                animator.Play("Idle");
+            }
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jump);
+            animator.Play("Jump");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.tag == "Coins")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
