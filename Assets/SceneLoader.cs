4882b3a5a9db9039ec8f6d17849983a854574179
Linnea <linnea@klangby.com>
2020-10-19 13:16:36 +0200

    Lager och startscene
    Fixat så att man kan lägga ljus mellan tiles och bakgrunden plus fixat att startscene

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+
+
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // Use a coroutine to load the Scene in the background
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Coins");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
