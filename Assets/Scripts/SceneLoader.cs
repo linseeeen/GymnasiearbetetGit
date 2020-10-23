032a5ec780d463918ae62c4792736941551ed4ea
Linnea <linnea@klangby.com>
2020-10-23 14:02:01 +0200

    Scenladdare fungerar
    Scenladdaren fungerar men måste justeras, lista på allt finns i discord.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,8 +9,7 @@ public class SceneLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene ();
-        
+       
     }
 
     // Update is called once per frame
@@ -47,27 +46,17 @@ public class SceneLoader : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        string sceneName = currentScene.name;
-        if (sceneName == "Coins")
+        if (other.transform.tag == "ScenLaddareCoins")
         {
-            if (other.transform.tag == "ScenLaddareCoins")
-            {
-                StartCoroutine(LoadSign());
-            }
+            StartCoroutine(LoadSign());
         }
-        else if (sceneName == "Skylt")
+        if (other.transform.tag == "ScenLaddareSkylt")
         {
-            if (other.transform.tag == "ScenLaddareSkylt")
-            {
-                StartCoroutine(LoadLight());
-            }
+            StartCoroutine(LoadLight());
         }
-        else if (sceneName == "Ljus")
+        if (other.transform.tag == "ScenLaddareLjus")
         {
-            if (other.transform.tag == "ScenLaddareLjus")
-            {
-                StartCoroutine(LoadSound());
-            }
+            StartCoroutine(LoadSound());
         }
     }
 }
