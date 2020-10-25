08a76a783bb5ae71b4d2ab4b0e694e03a33216be
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-10-25 15:25:51 +0100

    Ljud scene och finslipning

---
+++ Assets/Scripts/SpelaLjud.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelaLjud : MonoBehaviour
+{
+    
+    public AudioSource ljud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ljud = GetComponent<AudioSource> ();
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.transform.tag == "Player")
+        {
+            Debug.Log("SÄGÄGAS");
+            ljud.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+       if (other.transform.tag == "Player")
+        {
+            Debug.Log("SÄGÄGAS");
+            ljud.Stop();
+        } 
+    }
+}
