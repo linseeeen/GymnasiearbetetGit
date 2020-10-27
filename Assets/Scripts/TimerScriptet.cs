ecfc7c144ddbe1b85c93d983b4bbf83372867600
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-10-27 19:02:01 +0100

    timer + text för den (SAMPLESCENE)

---
+++ Assets/Scripts/TimerScriptet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScriptet : MonoBehaviour
+{
+    public Text timerText;
+
+    float timer = 0.0f;
+    bool timerStop = false;
+    int seconds;
+
+    void Update()
+    {
+        if(timerStop == false)
+        {
+            timer += Time.deltaTime;
+            // raderna under ändrar timern till sekunder, oklart om vi behöver det?
+            seconds = (int) timer;
+            Debug.Log(seconds);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        //Ändra tag till scenebytarens tag
+       if (other.transform.tag == "Coins")
+        {
+            timerStop = true;
+            string visaTimerText = seconds.ToString();
+            timerText.text = visaTimerText;
+        } 
+    }
+
+}
